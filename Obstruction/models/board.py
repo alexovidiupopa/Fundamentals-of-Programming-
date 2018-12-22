77bf10aeb39d27a40f0dd75b2fdc4170f5031e92
Alex Popa <popaalexovidiu@gmail.com>
2018-12-22 19:01:40 +0200

    still working...

--- Obstruction/models/board.py
+++ Obstruction/models/board.py
@@ -6,8 +6,21 @@ class Board(object):
     def __init__(self, width, height):
         self.__width = width
         self.__height = height
+        self.__board = []
+        self.__boardInitialize()
     
-    
+    def __boardInitialize(self):
+        for i in range(self.__width): 
+            row = []
+            for j in range(self.__height): 
+                row.append("[]")
+            self.__board.append(row)
+        self.boardPrint()
 
+    def boardPrint(self):
+        for i in range(self.__width): 
+            for j in range(self.__height): 
+                print(self.__board[i][j])
+            print("\n")
 
 
--- Obstruction/ui/gameSettings.py
+++ Obstruction/ui/gameSettings.py
@@ -1,21 +1,22 @@
 from validators import boardValidator
 from errors.errors import BoardError
-#import unittest
 from models.dimension import Dimension
 
 class Settings(object):
     
+    def __init__(self):
+        self.__boardValidator = boardValidator()
+        
     def getDimensions(self):
         print("Board width: ")
         try: 
             boardX = int(input())
             print("Board height: ")
             boardY = int(input())
-            boardValidator.validate(boardX,boardY)
+            self.__boardValidator.validate(boardX,boardY)
             return Dimension(boardX,boardY)
         except ValueError: 
             print("Incorrect dimensions! Must be integers! ")
         except BoardError as be: 
             print(be)
     
-
